f196c09738427d1c7f769263ffed1d864f3c8a64
灵空不帅 <1412878170@qq.com>
2021-06-01 20:27:04 +0800

    项目初始化

---
+++ education-admin-api/src/main/java/com/education/admin/api/EducationAdminApiApplication.java
@@ -0,0 +1,21 @@
+package com.education.admin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EducationAdminApiApplication {
+
+    @GetMapping
+    public String test(){
+        return "success";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationAdminApiApplication.class, args);
+    }
+
+}
---
+++ education-admin-api/src/test/java/com/education/admin/api/EducationAdminApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.education.admin.api;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationAdminApiApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
